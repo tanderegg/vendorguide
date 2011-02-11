82c86e33417e2cd6984e59e64b7c604773ba0a39
user <user@ubuntu.(none)>
2011-02-11 12:26:28 -0500

    Styled vendor index

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -4,4 +4,13 @@ class Vendor < ActiveRecord::Base
 
   validates_presence_of :name
   validates_presence_of :description
+
+  def rating
+    (reviews.average(:score) || "None").to_s
+  end
+
+  def tags
+    tags = ['Voter File', 'Analytics', 'Data Warehousing']
+    tags.join(', ')
+  end
 end
